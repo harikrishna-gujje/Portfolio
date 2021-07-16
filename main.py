ce8bfc7e4e21bfca56e7a7c0abca7e43b8a4f4d5
harikrishna-gujje <harikrishnagujje1729@gmail.com>
2021-07-16 07:38:42 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
+
+
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/components.html')
+def components():
+    return render_template('components.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run()
